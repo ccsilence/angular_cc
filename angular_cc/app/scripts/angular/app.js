782ab3260fa5c0d8cea2d869d8dcadd1980afd4e
ccsilence <1150082254@qq.com>
2018-03-22 23:24:57 +0800

    add route and home

--- angular_cc/app/scripts/angular/app.js
+++ angular_cc/app/scripts/angular/app.js
@@ -1,8 +1,14 @@
 var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {templateUrl: '/views/tpl/welcome.html', controller: 'WelcomeCtrl'})
-        .otherwise({redirectTo: '/'});
+    $routeProvider.when('/', {templateUrl: '/views/tpl/welcome.html', controller: 'WelcomeCtrl'});
+    $routeProvider.when('/login',{templateUrl:'/views/tpl/login.html',controller:'LoginCtrl'});
+    $routeProvider.when('/register',{templateUrl:'/views/tpl/register.html',controller:'RegisterCtrl'});
+    $routeProvider.when('/logout', {templateUrl:'/views/tpl/logout.html', controller:'LogoutCtrl'});
+    $routeProvider.otherwise({redirectTo:'/'});
+    /*
+      该处用到$localtion服务，
+      支持HTML5模式(/)和Hashbang模式（#/）
+    */
     $locationProvider.html5Mode(true);
 }]);
---
+++ angular_cc/app/scripts/angular/register.js
@@ -0,0 +1,6 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: ccsilence
+ * Date: 22/3/18
+ * Time: 9:49 PM
+ */
